bf75b00e72a75fc064447baabb7ff2d745030aae
GS <49228795+L10Messi10@users.noreply.github.com>
2023-05-06 11:40:58 +0800

    Update Products.cs

--- Sample1/Models/Products.cs
+++ Sample1/Models/Products.cs
@@ -10,9 +10,9 @@ namespace Sample1.Models
 {
     public class Products
     {
-        public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public string Image_1_Link { get; set; }
 
         public async Task<string> UploadImage(Stream img, string proname, string filename)
         {
@@ -28,10 +28,26 @@ namespace Sample1.Models
                 return "false";
             }
         }
+        
+        
+        ******************************************
+        var _users = new Users
+                    {
+                        Name = fullname,
+                        Description = randomNumber,
+                        Image_1_Link = _position,
+                       
+                    };
+                    await client
+                        .Child($"Users")
+                        .PostAsync(_users);
+
+        *************************************
 
         public async Task<bool> Save(FileResult mainimg)
         {
             var _mainimg = await UploadImage(await mainimg.OpenReadAsync(), "Studentname", $"_mainimg.png");
+            
             return true;
         }
     }
